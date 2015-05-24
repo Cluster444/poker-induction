4f0b983807e1de9c4a251d3cff321e9a8328c975
Chris Boertien <chris.boertien@gmail.com>
2015-05-24 08:02:12 -0300

    init repo
    Signed-off-by: Chris Boertien <chris.boertien@gmail.com>

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='poker-induction',
+      version='0.1',
+      description="Learning the rules of poker",
+      license="MIT",
+      author='Chris Boertien',
+      author_email='chris.boertien@gmail.com',
+      url='http://aktionlab.com',
+      package_dir = {'':'src'})
